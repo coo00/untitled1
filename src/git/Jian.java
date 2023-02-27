5de58c1e25f965e7abddaa6ca0d7722a627990a9
ll <bb@qq.com>
2023-02-27 13:55:07 +0800

    third commit

--- src/git/Jian.java
+++ src/git/Jian.java
@@ -4,6 +4,7 @@ public class Jian {
     public static  void main(String []args){
         System.out.println("hello word");
         System.out.println("hello git");
+        System.out.println("hello word");
 
     }
 }
